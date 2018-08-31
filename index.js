e66523c1273618b216f2a102153821799c4bbca3
Rejiejay <454766952@qq.com>
2018-08-31 17:53:49 +0800

    add register-input

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ window.onload = function () {
 // 配置类
 var config = (function () { // 匿名函数自执行
 	// 判断是否测试环境	
-	if ( window.location.origin === 'file://' || window.location.host === 'store.demo.ichebaoyang.com' ) {
+	if (window.location.origin === 'file://' || window.location.host === 'store.demo.ichebaoyang.com') {
 		// 测试环境
 		return {
-	
+
 		}
 
 	} else {
 		// 线上生产环境
 		return {
-	
+
 		}
 	}
 })();
@@ -31,23 +31,23 @@ var init = {
 
 	initVue: function initVue() {
 		// 路由配置
-        var routes = [ 
-			{ 
-				path: '/', 
+		var routes = [
+			{
+				path: '/',
 				component: VmMain
-			}, { 
-				path: '/supplement', 
+			}, {
+				path: '/supplement',
 				component: VmSupplement
 			}
-        ];
+		];
 
 		// 初始化路由配置
-        var router = new VueRouter({ 
-          routes: routes
+		var router = new VueRouter({
+			routes: routes
 		});
 
 		// 初始化 Vue 到页面
-		this.vueMount = new Vue({ 
+		this.vueMount = new Vue({
 			router: router
 		});
 
@@ -56,7 +56,7 @@ var init = {
 };
 
 // ajaxs请求类
-var ajaxs = { 
+var ajaxs = {
     /**
      * 测试请求
      * @param {string} pageNo 分页
@@ -74,17 +74,17 @@ var ajaxs = {
 			// resolve(result.stores); // 返回测试数据
 
 			$.get("/api/storeCarWash")
-			.done(function (data) {
-				Vue.prototype.$indicator.close();
-				if (data && data.code == 200) {
-					resolve(data.stores);
-				} else {
-					reject('向服务器发起请求成功，但是查找所有获取门店列表数据有误, 原因: ' + JSON.stringify(data));
-				}
-			}).fail(function (data) {
-				Vue.prototype.$indicator.close();
-				reject('向服务器发起请求查找所有获取门店列表失败, 原因: ' + JSON.stringify(data));
-			});
+				.done(function (data) {
+					Vue.prototype.$indicator.close();
+					if (data && data.code == 200) {
+						resolve(data.stores);
+					} else {
+						reject('向服务器发起请求成功，但是查找所有获取门店列表数据有误, 原因: ' + JSON.stringify(data));
+					}
+				}).fail(function (data) {
+					Vue.prototype.$indicator.close();
+					reject('向服务器发起请求查找所有获取门店列表失败, 原因: ' + JSON.stringify(data));
+				});
 		});
 	},
 };
@@ -93,10 +93,15 @@ var VmMain = { // 注册 Vue类
 	template: '#register',
 
 	data: function data() {
-		return {};
+		return {
+			// 手机号码
+			phoneValue: '',
+			// 验证号码
+			verifyNumber: '',
+		};
 	},
 
-	mounted: function mounted() {},
+	mounted: function mounted() { },
 
 	methods: {},
 };
@@ -120,7 +125,7 @@ var VmSupplement = { // 完善车辆信息 Vue类
 // 抽象的方法
 var utils = {
 	// 获取URL参数
-    loadPageVar: function loadPageVar(sVar) {
-        return decodeURI(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURI(sVar).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-    },
+	loadPageVar: function loadPageVar(sVar) {
+		return decodeURI(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURI(sVar).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+	},
 };
